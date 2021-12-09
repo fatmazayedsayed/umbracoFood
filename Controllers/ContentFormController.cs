6ee84061094d3539955c515ff2ff1432be5d3db3
Fatma zayed <Fatma.Sayed@gbrands.com>
2021-12-09 11:15:22 +0200

    video #6 send email

---
+++ Controllers/ContentFormController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using umbracoFood.Models;
+
+namespace umbracoFood.Controllers
+{
+    public class ContentFormController:SurfaceController
+    {
+        [HttpPost]
+        public ActionResult SubmitForm(ContentFormModel model)
+        {
+            MailMessage mail = new MailMessage(model.Email, "fatam.sayed@gbrands.com");
+            mail.Subject = model.Subject;
+            mail.Body = model.Message;
+
+            SmtpClient smtpClient= new SmtpClient("127.0.0.1",25);
+            smtpClient.Send(mail);
+
+            return RedirectToCurrentUmbracoPage();
+
+        }
+    }
+}
